f7ea6a230bce8af19ca3b56869fb797c3c30501d
Crystalaalx <crystalaalx@163.com>
2020-07-15 01:06:36 +0800

    邮箱支持

--- mail.php
+++ mail.php
@@ -19,51 +19,51 @@ $message= $_POST['message'];
 echo smtpmailer($from,$fullName,$message);  
 
 function smtpmailer($from, $from_name, $body) {
-	$mail = new PHPMailer();
-//	$mail->SMTPDebug = 1;
-	$mail->isSMTP();
-	$mail->SMTPAuth = true;
-	$mail->Host = 'smtp.163.com';
-	$mail->SMTPSecure = 'ssl';
-	$mail->Port = 465;
-	$mail->CharSet = 'UTF-8';
-	$mail->FromName = '发件人昵称';
-	$mail->Username = 'crystalaalx@163.com';
-	$mail->Password = 'TPPWOAYUFRCGFOAI';
-	$mail->SetFrom($from, $from_name);
-	$mail->isHTML(true);
-	$mail->addAddress('jiujun0520@163.com');
-	$mail->Subject = '来自www.sujiujun.com的邮件';
-	$mail->Body = '<h1> $body </h1>';
-//	$mail->addAttachment('./example.pdf');
-	$status = $mail->send();
-	echo $status;
-//	global $SMTPSERVER,$SMTPPORT,$SMTPUSER,$SMTPPASSWORD,$ADMINEMAIL,$ADMINNAME;
-//	global $SUBJECT,$EMAILFAILERROR,$EMAILSUCCESSMSG;
-//	global $error;
-//	readMailSettings();
-//	$to = $ADMINEMAIL;
-//	$mail = new PHPMailer();  // create a new object
-//	$mail->IsSMTP(); // enable SMTP
-//	$mail->IsMail();//enable email through php mail();
-//    $mail->IsHTML(true);
+//	$mail = new PHPMailer();
+//	$mail->SMTPDebug = 0;
+//	$mail->isSMTP();
+//	$mail->SMTPAuth = true;
+//	$mail->Host = 'smtp.163.com';
+//	$mail->SMTPSecure = 'ssl';
+//	$mail->Port = 465;
+//	$mail->CharSet = 'UTF-8';
+//	$mail->FromName = '发件人昵称';
+//	$mail->Username = 'crystalaalx@163.com';
+//	$mail->Password = 'TPPWOAYUFRCGFOAI';
 //	$mail->SetFrom($from, $from_name);
-//    $mail->Subject = $SUBJECT;
-//	$mail->Body = $body;
-//	$mail->AddAddress($to);
-//
-//	if(!$mail->Send())
-//    {
-//        echo "MAIL_ERROR ". $EMAILFAILERROR;
-//        return false;
-//    }
-//	else
-//    {
-//        echo "{";
-//        echo "\"MAIL_SUCCESS\":\"" .$EMAILSUCCESSMSG . "\",\n";
-//        echo "\"result\": \"true\"\n";
-//        echo "}";
-//    }
+//	$mail->isHTML(true);
+//	$mail->addAddress('jiujun0520@163.com');
+//	$mail->Subject = '来自www.sujiujun.com的邮件';
+//	$mail->Body = '<h1> $body </h1>';
+//	$status = $mail->send();
+//	echo $status;
+
+	global $SMTPSERVER,$SMTPPORT,$SMTPUSER,$SMTPPASSWORD,$ADMINEMAIL,$ADMINNAME;
+	global $SUBJECT,$EMAILFAILERROR,$EMAILSUCCESSMSG;
+	global $error;
+	readMailSettings();
+	$to = $ADMINEMAIL;
+	$mail = new PHPMailer();  // create a new object
+	$mail->IsSMTP(); // enable SMTP
+	$mail->IsMail();//enable email through php mail();
+    $mail->IsHTML(true);
+	$mail->SetFrom($from, $from_name);
+    $mail->Subject = $SUBJECT;
+	$mail->Body = $body;
+	$mail->AddAddress($to);
+
+	if(!$mail->Send())
+    {
+        echo "MAIL_ERROR ". $EMAILFAILERROR;
+        return false;
+    }
+	else
+    {
+        echo "{";
+        echo "\"MAIL_SUCCESS\":\"" .$EMAILSUCCESSMSG . "\",\n";
+        echo "\"result\": \"true\"\n";
+        echo "}";
+    }
 }
 function readMailSettings()
 {
@@ -73,6 +73,7 @@ function readMailSettings()
 	while(!feof($file))
 	{
 		$setting = explode(':',fgets($file));
+		print_r($setting);
 		switch($setting[0])
 		{
 			case 'SMTPSERVER':
